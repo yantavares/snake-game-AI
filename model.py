65a365bfa8d9a306f611905f08ae6fc466ed68b7
yantavares <yantdo1@gmail.com>
2023-10-17 15:02:52 -0300

    added structure from freecodecamp

--- model.py
+++ model.py
@@ -3,3 +3,8 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import os
+
+
+class Linear_QNet(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
